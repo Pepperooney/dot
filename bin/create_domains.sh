66337c4e321cba8a8de69381d1001a542d0333d4
Uwe Schwarz <starturtle@icloud.com>
2013-11-01 10:05:07 +0100

    create_domains: added sorting

--- bin/create_domains.sh
+++ bin/create_domains.sh
@@ -4,4 +4,4 @@ p=/srv
 depth=1
 dest=/srv/starturtle.eu.org/domains
 
-find "$p" -mindepth 1 -maxdepth 1 -type d -printf "%f\n" > "$dest"
+find "$p" -mindepth 1 -maxdepth 1 -type d -printf "%f\n" | sort > "$dest"
