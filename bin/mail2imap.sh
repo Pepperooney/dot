3a52fafa8f2e3b6c907c05aac5ef4604f51bbe85
Uwe Schwarz <uwe@erde.lu>
2014-02-03 14:21:32 +0100

    mail2imap: easier version

--- bin/mail2imap.sh
+++ bin/mail2imap.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
 # global
-logfile=$HOME/log/mail2imap.log
 folder=Archiv
+
+# backup to imap.mail.yahoo.com/Archive
+host=imap.mail.yahoo.com
+usr=star.turtle@yahoo.de
+pass="$(grep "machine $host .*login $usr" ~/.netrc | grep -o "password.*" | sed 's/password *//')"
 
 # save mail
 tmp=$(mktemp)
 cat > $tmp
 
-# init
-y=$(date +%Y)
-
-# backup to imap.mail.yahoo.com/Archive<year>
-host=imap.mail.yahoo.com
-usr=star.turtle@yahoo.de
-pass="$(grep "machine $host .*login $usr" ~/.netrc | grep -o "password.*" | sed 's/password *//')"
-
-echo -n "$(head -n 1 $tmp): " >> $logfile
-mailtool -tofolder "$folder$y" -copyto "imaps://$usr:$pass@$host/novalidate-cert" mbox:$tmp >> $logfile
+mailtool -tofolder "$folder" -copyto "imaps://$usr:$pass@$host/novalidate-cert" mbox:$tmp
 
 rm -f $tmp
