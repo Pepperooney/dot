658a4cb38c13ab9b246745845972f0143f671fb9
Uwe Schwarz <uwe@erde.lu>
2014-02-09 21:59:39 +0100

    bitstamp-btceur: included target and time for last read

--- bin/bitstamp-btceur.sh
+++ bin/bitstamp-btceur.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+target=/srv/erde.lu/mine/btceur.tsv
+
 btcusd=$(curl -s https://www.bitstamp.net/api/ticker/ | ~/bin/json.sh -b | awk '/ask/{print substr($2,2,length($2)-2)}')
 usdeur=$(curl -s https://www.bitstamp.net/api/eur_usd/ | ~/bin/json.sh -b | awk '/sell/{print substr($2,2,length($2)-2)}')
 
-echo "scale=4;$btcusd/$usdeur" | bc
+btceur=$(echo "scale=4;$btcusd/$usdeur" | bc 2>/dev/null)
+
+if [ $? -eq 0 ]; then
+    printf "%s\t%s %s\n" $btceur $(date "+%F %T") > $target
+fi
