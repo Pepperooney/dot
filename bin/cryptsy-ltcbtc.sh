6bae09113e6e0d87b261c894cf82194ab49f9b13
Uwe Schwarz <uwe@erde.lu>
2014-02-12 09:09:18 +0100

    cryptsy

--- bin/cryptsy-ltcbtc.sh
+++ bin/cryptsy-ltcbtc.sh
@@ -4,7 +4,7 @@ target=/srv/erde.lu/mine/ltcbtc.tsv
 
 ltcbtc="$(curl -s 'http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=3' 2>/dev/null | cut -d, -f 4 | cut -d\" -f4)"
 
-echo "$ltcbtc" | grep -q '^[0-9.]*$'
+echo "$ltcbtc" | grep -q '^[0-9.][0-9.]*$'
 if [ $? -eq 0 ]; then
     printf "%s\t%s %s\n" $ltcbtc $(date "+%F %T") > $target
 fi
