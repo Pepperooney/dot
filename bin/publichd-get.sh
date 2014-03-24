82675f88379f8d200e41f1a693945a92cf164f05
Uwe Schwarz <uwe@erde.lu>
2014-03-24 07:32:47 +0100

    publichd: refined get-script to silently drop errors

--- bin/publichd-get.sh
+++ bin/publichd-get.sh
@@ -6,7 +6,9 @@ if [ ! -f $log ]; then
     touch $log
 fi
 
-curl -s "https://publichd.se/rss.php" | xml2 | egrep '/item/title|/item/torrent/magnetURI'  | cut -d= -f2- | while read t; do
+xml="$(curl -s "https://publichd.se/rss.php" | xml2 2>/dev/null)"
+if [ $? -ne 0 ]; then exit 1; fi
+echo "$xml" | egrep '/item/title|/item/torrent/magnetURI'  | cut -d= -f2- | while read t; do
     t="$(echo "$t" | cut -d' ' -f2-)"
     read r
     if ! grep -qF "$t	$r" $log; then
