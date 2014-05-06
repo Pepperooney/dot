c3a0b6b465b851785af2b4660bc8975343fc4031
Uwe Schwarz <uwe@erde.lu>
2014-05-06 11:00:54 +0200

    ip_check: test for new ip of pi.erde.lu

---
+++ bin/ip_new.zsh
@@ -0,0 +1,15 @@
+#!/usr/bin/zsh
+
+atv="/etc/nginx/sites-enabled/appletv"
+ip="$1"
+
+hog_ip="22c9:d0ff:fe8a:640d"
+hog_key="3hum3KnQEzWGAtTt"
+
+range="$(echo "$ip" | cut -d: -f-4)"
+
+# nginx
+sed -i "s/^.*HOME$/  allow                 $range::\/64; # HOME/" "$atv"
+
+# hog
+curl -6kso /dev/null "https://hog.erde.lu:$hog_key@dyn.dns.he.net/nic/update?hostname=hog.erde.lu&myip=$range:$hog_ip"
---
+++ bin/ip_test.zsh
@@ -0,0 +1,19 @@
+#!/usr/bin/zsh
+
+old=~/.ip_pi
+rr="AAAA"
+h="pi.erde.lu"
+run=~/dot/bin/ip_new.zsh
+
+if [ ! -f "$old" ]; then touch "$old"; fi
+
+ip_new="$(dig +short "$rr" "$h")"
+if [ $? -ne 0 ]; then exit; fi
+ip_old="$(<"$old")"
+
+
+if [ "$ip_old" != "$ip_new" ]; then
+    #echo "$h=$ip_new"
+    echo "$ip_new" > "$old"
+    $run "$ip_new"
+fi
